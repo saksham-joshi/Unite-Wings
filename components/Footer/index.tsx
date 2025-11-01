e1f383205b392a4ff8add9a2a189c6f4ab38f0c6
SAKSHAM JOSHI <social.sakshamjoshi@gmail.com>
2025-11-01 09:10:14 +0530

    Set the "Useful links" on the side

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -10,9 +10,8 @@ const Footer = () => {
       <footer
         className="wow fadeInUp relative z-10 bg-white pt-16 dark:bg-gray-dark md:pt-20 lg:pt-24"
         data-wow-delay=".1s"
-        align="center"
       >
-        <div className="container" align="center">
+        <div className="container">
           <div className="-mx-4 flex flex-wrap">
             <div className="w-full px-4 md:w-1/2 lg:w-4/12 xl:w-5/12">
               <div className="mb-12 max-w-[360px] lg:mb-16">
@@ -116,66 +115,7 @@ const Footer = () => {
                 </div>
               </div>
 
-              {/* <div className="mb-12 lg:mb-16">
-                <h2 className="mb-10 text-xl font-bold text-black dark:text-white">
-
-                  <ul>
-                    <li>
-                        <a
-                      href="/privacy-policy"
-                      className="mb-4 inline-block text-base text-body-color duration-300 hover:text-primary dark:text-body-color-dark dark:hover:text-primary"
-                    >
-                      Privacy Policy
-                  </a>
-                    </li>
-
-                    <li>
-                        <a
-                      href="/verify"
-                      className="mb-4 inline-block text-base text-body-color duration-300 hover:text-primary dark:text-body-color-dark dark:hover:text-primary"
-                        >
-                      Verify Certificates
-                    </a>
-                    </li>
-                  </ul>
-                </h2>
-              </div>
-            </div>  */}
-
-              <div className="w-full px-4 sm:w-1/2 md:w-1/2 lg:w-2/12 xl:w-2/12">
-                <div className="mb-12 lg:mb-16">
-                  <h2 className="mb-10 text-xl font-bold text-black dark:text-white">
-                    Useful Links
-                  </h2>
-                  <ul>
-                    <li>
-                      <a
-                        href="/privacy-policy"
-                        className="mb-4 inline-block text-base text-body-color duration-300 hover:text-primary dark:text-body-color-dark dark:hover:text-primary"
-                      >
-                        Privacy Policy
-                      </a>
-                    </li>
-                    <li>
-                      <a
-                        href="/verify"
-                        className="mb-4 inline-block text-base text-body-color duration-300 hover:text-primary dark:text-body-color-dark dark:hover:text-primary"
-                      >
-                        Verify Certificate
-                      </a>
-                    </li>
-
-                    <li>
-                      <a
-                        href={VALUES.URL.GITHUB_URL}
-                        className="mb-4 inline-block text-base text-body-color duration-300 hover:text-primary dark:text-body-color-dark dark:hover:text-primary"
-                      >
-                        GitHub
-                      </a>
-                    </li>
-                  </ul>
-                </div>
-              </div>
+              
               {/* 
             <div className="w-full px-4 sm:w-1/2 md:w-1/2 lg:w-2/12 xl:w-2/12">
               <div className="mb-12 lg:mb-16">
@@ -248,6 +188,41 @@ const Footer = () => {
               </div>
             </div>*/}
             </div>
+
+            <div className="w-full px-4 sm:w-1/2 md:w-1/2 lg:w-2/12 xl:w-2/12">
+                <div className="mb-12 lg:mb-16">
+                  <h2 className="mb-10 text-xl font-bold text-black dark:text-white">
+                    Useful Links
+                  </h2>
+                  <ul>
+                    <li>
+                      <a
+                        href="/privacy-policy"
+                        className="mb-4 inline-block text-base text-body-color duration-300 hover:text-primary dark:text-body-color-dark dark:hover:text-primary"
+                      >
+                        Privacy Policy
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        href="/verify"
+                        className="mb-4 inline-block text-base text-body-color duration-300 hover:text-primary dark:text-body-color-dark dark:hover:text-primary"
+                      >
+                        Verify Certificate
+                      </a>
+                    </li>
+
+                    <li>
+                      <a
+                        href={VALUES.URL.GITHUB_URL}
+                        className="mb-4 inline-block text-base text-body-color duration-300 hover:text-primary dark:text-body-color-dark dark:hover:text-primary"
+                      >
+                        GitHub
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
           </div>
 
           <div className="h-px w-full bg-gradient-to-r from-transparent via-[#D2D8E183] to-transparent dark:via-[#959CB183]"></div>
