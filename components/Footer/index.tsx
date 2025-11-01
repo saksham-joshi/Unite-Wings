e50c306932090983122e9fe2299176dc4c781342
SAKSHAM JOSHI <social.sakshamjoshi@gmail.com>
2025-11-01 08:54:33 +0530

    Formatted + Added: Useful links

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -2,7 +2,7 @@
 import { getImagePath } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
-import {VALUES,COLORS,IMAGES} from "@/lib/variables";
+import { VALUES, COLORS, IMAGES } from "@/lib/variables";
 
 const Footer = () => {
   return (
@@ -32,9 +32,15 @@ const Footer = () => {
                     height={30}
                   />
                 </Link>
-                <p className="mb-4 text-base leading-relaxed text-body-color dark:text-body-color-dark">CIN: U85500UT2025NPL019683</p>
+                <p className="mb-4 text-base leading-relaxed text-body-color dark:text-body-color-dark">
+                  CIN: U85500UT2025NPL019683
+                </p>
                 <p className="mb-8 text-base leading-relaxed text-body-color dark:text-body-color-dark">
-                  A registered non-profit (Section 8 Company) dedicated to bridging the gap between education and employability for India's youth. We are building the next generation of leaders through practical skills, mentorship, and industry-aligned training.
+                  A registered non-profit (Section 8 Company) dedicated to
+                  bridging the gap between education and employability for
+                  India's youth. We are building the next generation of leaders
+                  through practical skills, mentorship, and industry-aligned
+                  training.
                 </p>
                 <div className="flex items-center">
                   <a
@@ -43,14 +49,23 @@ const Footer = () => {
                     className="mr-6 text-body-color duration-300 hover:text-primary dark:text-body-color-dark dark:hover:text-primary"
                     onClick={(e) => e.preventDefault()}
                   >
-                    <svg 
+                    <svg
                       width="24"
                       height="24"
-                      viewBox="0 0 24 24" 
-                      className="fill-current">
-                      <path fillRule="evenodd" clipRule="evenodd" d="M12 18C15.3137 18 18 15.3137 18 12C18 8.68629 15.3137 6 12 6C8.68629 6 6 8.68629 6 12C6 15.3137 8.68629 18 12 18ZM12 16C14.2091 16 16 14.2091 16 12C16 9.79086 14.2091 8 12 8C9.79086 8 8 9.79086 8 12C8 14.2091 9.79086 16 12 16Z" />
-                      <path d="M18 5C17.4477 5 17 5.44772 17 6C17 6.55228 17.4477 7 18 7C18.5523 7 19 6.55228 19 6C19 5.44772 18.5523 5 18 5Z"/>
-                      <path fillRule="evenodd" clipRule="evenodd" d="M1.65396 4.27606C1 5.55953 1 7.23969 1 10.6V13.4C1 16.7603 1 18.4405 1.65396 19.7239C2.2292 20.8529 3.14708 21.7708 4.27606 22.346C5.55953 23 7.23969 23 10.6 23H13.4C16.7603 23 18.4405 23 19.7239 22.346C20.8529 21.7708 21.7708 20.8529 22.346 19.7239C23 18.4405 23 16.7603 23 13.4V10.6C23 7.23969 23 5.55953 22.346 4.27606C21.7708 3.14708 20.8529 2.2292 19.7239 1.65396C18.4405 1 16.7603 1 13.4 1H10.6C7.23969 1 5.55953 1 4.27606 1.65396C3.14708 2.2292 2.2292 3.14708 1.65396 4.27606ZM13.4 3H10.6C8.88684 3 7.72225 3.00156 6.82208 3.0751C5.94524 3.14674 5.49684 3.27659 5.18404 3.43597C4.43139 3.81947 3.81947 4.43139 3.43597 5.18404C3.27659 5.49684 3.14674 5.94524 3.0751 6.82208C3.00156 7.72225 3 8.88684 3 10.6V13.4C3 15.1132 3.00156 16.2777 3.0751 17.1779C3.14674 18.0548 3.27659 18.5032 3.43597 18.816C3.81947 19.5686 4.43139 20.1805 5.18404 20.564C5.49684 20.7234 5.94524 20.8533 6.82208 20.9249C7.72225 20.9984 8.88684 21 10.6 21H13.4C15.1132 21 16.2777 20.9984 17.1779 20.9249C18.0548 20.8533 18.5032 20.7234 18.816 20.564C19.5686 20.1805 20.1805 19.5686 20.564 18.816C20.7234 18.5032 20.8533 18.0548 20.9249 17.1779C20.9984 16.2777 21 15.1132 21 13.4V10.6C21 8.88684 20.9984 7.72225 20.9249 6.82208C20.8533 5.94524 20.7234 5.49684 20.564 5.18404C20.1805 4.43139 19.5686 3.81947 18.816 3.43597C18.5032 3.27659 18.0548 3.14674 17.1779 3.0751C16.2777 3.00156 15.1132 3 13.4 3Z"/>
+                      viewBox="0 0 24 24"
+                      className="fill-current"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        clipRule="evenodd"
+                        d="M12 18C15.3137 18 18 15.3137 18 12C18 8.68629 15.3137 6 12 6C8.68629 6 6 8.68629 6 12C6 15.3137 8.68629 18 12 18ZM12 16C14.2091 16 16 14.2091 16 12C16 9.79086 14.2091 8 12 8C9.79086 8 8 9.79086 8 12C8 14.2091 9.79086 16 12 16Z"
+                      />
+                      <path d="M18 5C17.4477 5 17 5.44772 17 6C17 6.55228 17.4477 7 18 7C18.5523 7 19 6.55228 19 6C19 5.44772 18.5523 5 18 5Z" />
+                      <path
+                        fillRule="evenodd"
+                        clipRule="evenodd"
+                        d="M1.65396 4.27606C1 5.55953 1 7.23969 1 10.6V13.4C1 16.7603 1 18.4405 1.65396 19.7239C2.2292 20.8529 3.14708 21.7708 4.27606 22.346C5.55953 23 7.23969 23 10.6 23H13.4C16.7603 23 18.4405 23 19.7239 22.346C20.8529 21.7708 21.7708 20.8529 22.346 19.7239C23 18.4405 23 16.7603 23 13.4V10.6C23 7.23969 23 5.55953 22.346 4.27606C21.7708 3.14708 20.8529 2.2292 19.7239 1.65396C18.4405 1 16.7603 1 13.4 1H10.6C7.23969 1 5.55953 1 4.27606 1.65396C3.14708 2.2292 2.2292 3.14708 1.65396 4.27606ZM13.4 3H10.6C8.88684 3 7.72225 3.00156 6.82208 3.0751C5.94524 3.14674 5.49684 3.27659 5.18404 3.43597C4.43139 3.81947 3.81947 4.43139 3.43597 5.18404C3.27659 5.49684 3.14674 5.94524 3.0751 6.82208C3.00156 7.72225 3 8.88684 3 10.6V13.4C3 15.1132 3.00156 16.2777 3.0751 17.1779C3.14674 18.0548 3.27659 18.5032 3.43597 18.816C3.81947 19.5686 4.43139 20.1805 5.18404 20.564C5.49684 20.7234 5.94524 20.8533 6.82208 20.9249C7.72225 20.9984 8.88684 21 10.6 21H13.4C15.1132 21 16.2777 20.9984 17.1779 20.9249C18.0548 20.8533 18.5032 20.7234 18.816 20.564C19.5686 20.1805 20.1805 19.5686 20.564 18.816C20.7234 18.5032 20.8533 18.0548 20.9249 17.1779C20.9984 16.2777 21 15.1132 21 13.4V10.6C21 8.88684 20.9984 7.72225 20.9249 6.82208C20.8533 5.94524 20.7234 5.49684 20.564 5.18404C20.1805 4.43139 19.5686 3.81947 18.816 3.43597C18.5032 3.27659 18.0548 3.14674 17.1779 3.0751C16.2777 3.00156 15.1132 3 13.4 3Z"
+                      />
                     </svg>
                   </a>
                   <a
@@ -99,56 +114,69 @@ const Footer = () => {
                     </svg>
                   </a>
                 </div>
-
-          
               </div>
 
-              <div className="mb-12 lg:mb-16">
+              {/* <div className="mb-12 lg:mb-16">
                 <h2 className="mb-10 text-xl font-bold text-black dark:text-white">
-                  <a
+
+                  <ul>
+                    <li>
+                        <a
                       href="/privacy-policy"
                       className="mb-4 inline-block text-base text-body-color duration-300 hover:text-primary dark:text-body-color-dark dark:hover:text-primary"
                     >
                       Privacy Policy
+                  </a>
+                    </li>
+
+                    <li>
+                        <a
+                      href="/verify"
+                      className="mb-4 inline-block text-base text-body-color duration-300 hover:text-primary dark:text-body-color-dark dark:hover:text-primary"
+                        >
+                      Verify Certificates
                     </a>
+                    </li>
+                  </ul>
                 </h2>
               </div>
-            </div> 
+            </div>  */}
 
-            {/* <div className="w-full px-4 sm:w-1/2 md:w-1/2 lg:w-2/12 xl:w-2/12">
-              <div className="mb-12 lg:mb-16">
-                <h2 className="mb-10 text-xl font-bold text-black dark:text-white">
-                  Useful Links
-                </h2>
-                <ul>
-                  <li>
-                    <a
-                      href="/blogs"
-                      className="mb-4 inline-block text-base text-body-color duration-300 hover:text-primary dark:text-body-color-dark dark:hover:text-primary"
-                    >
-                      Blog
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href="/pricing"
-                      className="mb-4 inline-block text-base text-body-color duration-300 hover:text-primary dark:text-body-color-dark dark:hover:text-primary"
-                    >
-                      Pricing
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href="/about"
-                      className="mb-4 inline-block text-base text-body-color duration-300 hover:text-primary dark:text-body-color-dark dark:hover:text-primary"
-                    >
-                      About
-                    </a>
-                  </li>
-                </ul>
+              <div className="w-full px-4 sm:w-1/2 md:w-1/2 lg:w-2/12 xl:w-2/12">
+                <div className="mb-12 lg:mb-16">
+                  <h2 className="mb-10 text-xl font-bold text-black dark:text-white">
+                    Useful Links
+                  </h2>
+                  <ul>
+                    <li>
+                      <a
+                        href="/privacy-policy"
+                        className="mb-4 inline-block text-base text-body-color duration-300 hover:text-primary dark:text-body-color-dark dark:hover:text-primary"
+                      >
+                        Privacy Policy
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        href="/verify"
+                        className="mb-4 inline-block text-base text-body-color duration-300 hover:text-primary dark:text-body-color-dark dark:hover:text-primary"
+                      >
+                        Verify Certificate
+                      </a>
+                    </li>
+
+                    <li>
+                      <a
+                        href={VALUES.URL.GITHUB_URL}
+                        className="mb-4 inline-block text-base text-body-color duration-300 hover:text-primary dark:text-body-color-dark dark:hover:text-primary"
+                      >
+                        GitHub
+                      </a>
+                    </li>
+                  </ul>
+                </div>
               </div>
-            </div>
-
+              {/* 
             <div className="w-full px-4 sm:w-1/2 md:w-1/2 lg:w-2/12 xl:w-2/12">
               <div className="mb-12 lg:mb-16">
                 <h2 className="mb-10 text-xl font-bold text-black dark:text-white">
@@ -219,12 +247,15 @@ const Footer = () => {
                 </ul>
               </div>
             </div>*/}
+            </div>
           </div>
 
           <div className="h-px w-full bg-gradient-to-r from-transparent via-[#D2D8E183] to-transparent dark:via-[#959CB183]"></div>
           <div className="py-8">
-            <p className="text-center text-base text-body-color dark:text-white">
-              © 2025 Unite Wings Educational Confederation. All Rights Reserved. | A Section 8 Company incorporated under the Companies Act, 2013
+            <p className="text-center text-base text-xs text-body-color dark:text-white">
+              © 2025 Unite Wings Educational Confederation. All Rights
+              Reserved. | A Section 8 Company incorporated under the Companies
+              Act, 2013
             </p>
           </div>
         </div>
@@ -369,7 +400,11 @@ const Footer = () => {
                 gradientUnits="userSpaceOnUse"
               >
                 <stop stopColor={COLORS.PRIMARY} stopOpacity="0" />
-                <stop offset="1" stopColor={COLORS.PRIMARY} stopOpacity="0.51" />
+                <stop
+                  offset="1"
+                  stopColor={COLORS.PRIMARY}
+                  stopOpacity="0.51"
+                />
               </linearGradient>
               <linearGradient
                 id="paint2_linear_94:889"
@@ -391,7 +426,11 @@ const Footer = () => {
                 gradientUnits="userSpaceOnUse"
               >
                 <stop stopColor={COLORS.PRIMARY} stopOpacity="0" />
-                <stop offset="1" stopColor={COLORS.PRIMARY} stopOpacity="0.51" />
+                <stop
+                  offset="1"
+                  stopColor={COLORS.PRIMARY}
+                  stopOpacity="0.51"
+                />
               </linearGradient>
               <linearGradient
                 id="paint4_linear_94:889"
@@ -413,7 +452,11 @@ const Footer = () => {
                 gradientUnits="userSpaceOnUse"
               >
                 <stop stopColor={COLORS.PRIMARY} stopOpacity="0" />
-                <stop offset="1" stopColor={COLORS.PRIMARY} stopOpacity="0.51" />
+                <stop
+                  offset="1"
+                  stopColor={COLORS.PRIMARY}
+                  stopOpacity="0.51"
+                />
               </linearGradient>
             </defs>
           </svg>
